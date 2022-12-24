14fdc3417aaa766ad5e8dd627778b6a08c6fa0ab
Анастасія Савченко <savyle4ka13@gmail.com>
2022-12-24 16:55:55 +0200

    add 1 hw

---
+++ js/01-gallery.js
@@ -0,0 +1,95 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.addEventListener("click", onImgClick);
+
+const markupGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", markupGallery);
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let originalImg = e.target.dataset.source;
+  console.log(originalImg);
+
+  const instance = basicLightbox.create(`
+    <img src=${originalImg} width="800" height="600">
+`);
+
+  instance.show();
+
+  document.addEventListener("keydown", onEscPushClose);
+
+  function onEscPushClose(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+console.log(galleryItems);
+
+// const galleryRef = document.querySelector(".gallery");
+
+// const markupGalleryEl = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `<div class="gallery__item">
+//   <a class="gallery__link" href=${original}>
+//     <img
+//       class="gallery__image"
+//       src=${preview}
+//       data-source=${original}
+//       alt=${description}
+//     />
+//   </a>
+// </div>`;
+//   })
+//   .join("");
+
+// galleryRef.insertAdjacentHTML("beforeend", markupGalleryEl);
+
+// galleryRef.addEventListener("click", onImgClick);
+
+// let originalImg;
+// const instance = basicLightbox.create(
+//   `<img src=${originalImg} width="800" height="600"> `
+// );
+
+// function onImgClick(e) {
+//   e.preventDefault();
+//   if (e.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+//   originalImg = e.target.dataset.source;
+//   console.log(originalImg);
+
+//   instance.show();
+// }
+
+// document.addEventListener("keydown", onEscPushClose);
+
+// function onEscPushClose(e) {
+//   if (e.code === "Escape") {
+//     instance.close();
+//   }
+// }
